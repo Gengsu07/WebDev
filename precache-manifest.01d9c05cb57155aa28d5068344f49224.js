0199da664a779c39290278387d86dc76e42a19b4
Sugeng Wahyudi <sugengdatascience@gmail.com>
2022-10-12 06:23:08 +0700

    Updates

---
+++ precache-manifest.01d9c05cb57155aa28d5068344f49224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4cddb24b5476b4991f",
+    "url": "/Gengsu07/RoboFriends/static/css/main.1d8a9739.chunk.css"
+  },
+  {
+    "revision": "4a4cddb24b5476b4991f",
+    "url": "/Gengsu07/RoboFriends/static/js/main.ccde41b0.chunk.js"
+  },
+  {
+    "revision": "d3ad7d2e8e2697cccbea",
+    "url": "/Gengsu07/RoboFriends/static/js/runtime~main.7fd3b542.js"
+  },
+  {
+    "revision": "f4a3f57c996eb14b4bc0",
+    "url": "/Gengsu07/RoboFriends/static/css/2.ff592d74.chunk.css"
+  },
+  {
+    "revision": "f4a3f57c996eb14b4bc0",
+    "url": "/Gengsu07/RoboFriends/static/js/2.a57b84b1.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/Gengsu07/RoboFriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "bdf02236b1a7a561d61f14a0bbc0317e",
+    "url": "/Gengsu07/RoboFriends/index.html"
+  }
+];
